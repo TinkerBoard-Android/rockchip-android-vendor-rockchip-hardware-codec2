7b7739b158c8ff081cd8e7aab79612329029e5fa
Bian Jin chen <kenjc.bian@rock-chips.com>
2022-08-22 06:57:47 +0000

    codec2 components: fix subscribed indices setter
    Cherry-picked from media.swcodec codec2.
    
    Bug: 222205463
    Test: boot
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I8d3e999387739ed942df6b31b7bfa2e205ebf489

--- component/base/C2RKInterface.cpp
+++ component/base/C2RKInterface.cpp
@@ -27,6 +27,14 @@ namespace android {
 
 /* SimpleInterface */
 
+static C2R SubscribedParamIndicesSetter(
+        bool mayBlock, C2InterfaceHelper::C2P<C2SubscribedParamIndicesTuning> &me) {
+    (void)mayBlock;
+    (void)me;
+
+    return C2R::Ok();
+}
+
 C2RKInterface<void>::BaseParams::BaseParams(
         const std::shared_ptr<C2ReflectorHelper> &reflector,
         C2String name,
@@ -175,7 +183,7 @@ C2RKInterface<void>::BaseParams::BaseParams(
             .withDefault(C2SubscribedParamIndicesTuning::AllocShared(0u))
             .withFields({ C2F(mSubscribedParamIndices, m.values[0]).any(),
                           C2F(mSubscribedParamIndices, m.values).any() })
-            .withSetter(Setter<C2SubscribedParamIndicesTuning>::NonStrictValuesWithNoDeps)
+            .withSetter(SubscribedParamIndicesSetter)
             .build());
 
     /* TODO
